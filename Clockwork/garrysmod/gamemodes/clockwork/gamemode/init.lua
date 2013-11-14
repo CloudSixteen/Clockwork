b24557195989a7daacd7c86603729b9abdbb5f73
duck <judgeduckster@gmail.com>
2013-11-13 17:09:19 -0800

    Added check if CloudAuthX exists and if the system is Linux

--- Clockwork/garrysmod/gamemodes/clockwork/gamemode/init.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/gamemode/init.lua
@@ -1,10 +1,18 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 
 	Clockwork was created by Conna Wiles (also known as kurozael.)
 	https://creativecommons.org/licenses/by-nc-nd/3.0/legalcode
 --]]
+
+if (system.IsLinux()) then
+	ErrorNoHalt("[Clockwork] Clockwork is incompatible with Linux at this time!\n");
+end;
+
+if (!file.Exists("bin/gmsv_cloudauthx_win32.dll", "LUA")) then
+	Error("[Clockwork] CloudAuthX is missing! (garrysmod/lua/bin).\n");
+end;
 
 --[[ Require the CloudAuthX authentication system. --]]
 require("cloudauthx");
