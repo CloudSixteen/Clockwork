f3350d04a2184b98fb43dd5a4c52cb805aa1cc9a
xRJx <jonathan.droogh@gmail.com>
2013-11-13 20:49:21 -0500

    Create cl_imagebutton.lua

---
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/derma/cl_imagebutton.lua
@@ -0,0 +1,145 @@
+--[[
+	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	https://creativecommons.org/licenses/by-nc-nd/3.0/legalcode
+--]]
+
+local Clockwork = Clockwork;
+local Color = Color;
+local vgui = vgui;
+local math = math;
+
+local PANEL = {};
+
+-- A function to set whether the panel is disabled.
+function PANEL:SetDisabled(disabled)
+	self.Disabled = disabled;
+end;
+
+-- A function to get whether the panel is disabled.
+function PANEL:GetDisabled()
+	return self.Disabled;
+end;
+
+-- A function to set whether the panel is depressed.
+function PANEL:SetDepressed(depressed)
+	self.Depressed = depressed;
+end;
+
+-- A function to get whether the panel is depressed.
+function PANEL:GetDepressed()
+	return self.Depressed;
+end;
+
+-- A function to set whether the panel is hovered.
+function PANEL:SetHovered(hovered)
+	self.Hovered = hovered;
+end;
+
+-- A function to get whether the panel is hovered.
+function PANEL:GetHovered()
+	return self.Hovered;
+end;
+
+-- Called when the cursor has entered the panel.
+function PANEL:OnCursorEntered()
+	if (!self:GetDisabled()) then
+		self:SetHovered(true);
+	end;
+	
+	DImage.ApplySchemeSettings(self);
+end;
+
+-- Called when the cursor has exited the panel.
+function PANEL:OnCursorExited()
+	self:SetHovered(false);
+	DImage.ApplySchemeSettings(self);
+end;
+
+-- Called when the mouse is pressed.
+function PANEL:OnMousePressed(code)
+	self:MouseCapture(true);
+	self:SetDepressed(true);
+end;
+
+-- Called when the mouse is released.
+function PANEL:OnMouseReleased(code)
+	self:MouseCapture(false);
+	
+	if (!self:GetDepressed()) then
+		return;
+	end;
+	
+	self:SetDepressed(false);
+	
+	if (!self:GetHovered()) then
+		return;
+	end;
+	
+	if (code == MOUSE_LEFT and self.DoClick
+	and !self:GetDisabled()) then
+		self.DoClick(self);
+	end;
+end;
+
+-- A function to make the panel fade out.
+function PANEL:FadeOut(speed, Callback)
+	self.animation = Derma_Anim("Fade Panel", self, function(panel, animation, delta, data)
+		panel:SetAlpha(255 - (delta * 255));
+		
+		if (animation.Finished) then
+			panel:SetVisible(false);
+				if (Callback) then
+					Callback();
+				end;
+			self.animation = nil;
+		end;
+	end);
+	
+	if (self.animation) then
+		self.animation:Start(speed);
+	end;
+	
+	Clockwork.option:PlaySound("rollover");
+end;
+
+-- A function to make the panel fade in.
+function PANEL:FadeIn(speed, Callback)
+	self.animation = Derma_Anim("Fade Panel", self, function(panel, animation, delta, data)
+		panel:SetAlpha(delta * 255);
+		
+		if (animation.Finished) then
+			if (Callback) then
+				Callback();
+			end;
+			
+			self.animation = nil;
+		end;
+	end);
+	
+	if (self.animation) then
+		self.animation:Start(speed);
+	end;
+	
+	Clockwork.option:PlaySound("click_release");
+	self:SetVisible(true);
+end;
+
+-- Called every frame.
+function PANEL:Think()
+	if (self.animation) then
+		self.animation:Run();
+	end;
+end;
+
+-- A function to set the panel's Callback.
+function PANEL:SetCallback(Callback)
+	self.DoClick = function(button)
+		Clockwork.option:PlaySound("click");
+		Callback(button);
+	end;
+end;
+
+vgui.Register("cwImageButton", PANEL, "DImage");
