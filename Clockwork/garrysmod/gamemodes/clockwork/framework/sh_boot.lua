2a078f5bfaa16dfe157543d283180de65d4a3cf4
Conna Wiles <kurozael@gmail.com>
2014-04-01 21:01:57 +0100

    Updated version to 0.91 in the sh_boot file.

--- Clockwork/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
@@ -17,14 +17,14 @@ local game = game;
 Clockwork = Clockwork or GM;
 Clockwork.ClockworkFolder = Clockwork.ClockworkFolder or GM.Folder;
 Clockwork.SchemaFolder = Clockwork.SchemaFolder or GM.Folder;
-Clockwork.KernelVersion = "0.9";
+Clockwork.KernelVersion = "0.91";
 Clockwork.Website = "http://kurozael.com";
 Clockwork.Author = "kurozael";
 Clockwork.Email = "kurozael@gmail.com";
 Clockwork.Name = "Clockwork";
 
 --[[ Check if we are using the right CloudAX version. --]]
-if (SERVER and CloudAuthX.GetVersion() < 3) then
+if (SERVER and CloudAuthX.GetVersion() < 5) then
 	for i = 1, 3 do
 		Error("[CloudAuthX] Clockwork requires an updated CloudAuthX .dll!\n");
 	end;
