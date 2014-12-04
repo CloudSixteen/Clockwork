a24ec6c770161d5051ed6ccbe41c927f784ee98d
VortixDev <ryansaridar@hotmail.co.uk>
2014-12-04 18:56:55 +0000

    Update cl_plugin.lua

--- Clockwork/garrysmod/gamemodes/clockwork/plugins/stamina/plugin/cl_plugin.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/stamina/plugin/cl_plugin.lua
@@ -6,6 +6,6 @@
 	http://cloudsixteen.com/license/clockwork.html
 --]]
 
-Clockwork.config:AddToSystem("Stamina Drain Scale", "stam_drain_scale", "Determines how fast stamina should drain.", 0, 1, 3);
 Clockwork.config:AddToSystem("Stamina Regeneration Scale", "stam_regen_scale", "Determines how fast stamina should regenerate.", 0, 1, 3);
+Clockwork.config:AddToSystem("Stamina Drain Scale", "stam_drain_scale", "Determines how fast stamina should drain.", 0, 1, 3);
 Clockwork.config:AddToSystem("Breathing Volume", "breathing_volume", "The volume for breathing when a player's stamina is low.", 0, 100);
