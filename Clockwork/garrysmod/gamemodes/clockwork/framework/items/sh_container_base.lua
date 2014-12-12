82df7d2a94951c3aa1810b736dfbf5524021d91c
VortixDev <ryansaridar@hotmail.co.uk>
2014-12-12 10:38:23 +0000

    Update sh_container_base.lua

--- Clockwork/garrysmod/gamemodes/clockwork/framework/items/sh_container_base.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/items/sh_container_base.lua
@@ -1,6 +1,8 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	Â© 2014 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
 --]]
 
 local ITEM = Clockwork.item:New();
