eb5d61df86fd7e7a96ca6e6f07b712f73a447054
Anna Craig <annacraig1455@hotmail.com>
2015-01-23 10:47:15 -0500

    StaticEnts
    Removed the obsolete static prop plugin and added static entities, which
    can load from static prop save files and convert to new format.

--- Clockwork/garrysmod/gamemodes/clockwork/plugins/staticents/plugin/sh_plugin.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/staticents/plugin/sh_plugin.lua
@@ -10,8 +10,24 @@
 	You don't have to do this, but I think it's nicer.
 	Alternatively, you can simply use the PLUGIN variable.
 --]]
-PLUGIN:SetGlobalAlias("cwStaticProps");
+PLUGIN:SetGlobalAlias("cwStaticEnts");
 
 --[[ You don't have to do this either, but I prefer to seperate the functions. --]]
+Clockwork.kernel:IncludePrefixed("cl_plugin.lua");
 Clockwork.kernel:IncludePrefixed("sv_plugin.lua");
 Clockwork.kernel:IncludePrefixed("sv_hooks.lua");
+
+-- A function to check if an entity can be static.
+function cwStaticEnts:CanStatic(entity)
+	if (entity:IsValid()) then
+		local class = entity:GetClass();
+		
+		if (class == "prop_vehicle_airboat" or class == "prop_vehicle_jeep" or class == "Jeep" or class == "Airboat") then
+			return "nope";
+		end;
+		
+		return class;
+	end;
+	
+	return false;
+end;
