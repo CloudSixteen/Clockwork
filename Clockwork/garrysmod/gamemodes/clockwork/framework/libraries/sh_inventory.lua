CloudSixteen/Clockwork

commit 0fe5c21fa07d535c3ad4fc1e9721e00cd1f28789

Adds quantity to AddInstance

Clockwork/garrysmod/gamemodes/clockwork/framework/libraries/sh_inventory.lua
@@ -140,7 +140,7 @@ function Clockwork.inventory:IsEmpty(inventory)
 end;
 
 -- A function to add an instance to a table.
-function Clockwork.inventory:AddInstance(inventory, itemTable)
+function Clockwork.inventory:AddInstance(inventory, itemTable, quantity)
 	if (itemTable == nil) then
 		return false;
 	end;
@@ -154,7 +154,11 @@ function Clockwork.inventory:AddInstance(inventory, itemTable)
 		inventory[itemTable("uniqueID")] = {};
 	end;
 	
-	inventory[itemTable("uniqueID")][itemTable("itemID")] = itemTable;
+	for i = 1, (quantity or 1) do
+		itemTable = Clockwork.item:CreateInstance(itemTable("uniqueID"));
+		inventory[itemTable("uniqueID")][itemTable("itemID")] = itemTable;
+	end;
+	
 	return itemTable;
 end;
 

commit 64c7fb258b5956c11be483d78907a6190367871c

Fixes items not registering properly - caused by 0fe5c21f

Clockwork/garrysmod/gamemodes/clockwork/framework/libraries/sh_inventory.lua
@@ -141,6 +141,8 @@ end;
 
 -- A function to add an instance to a table.
 function Clockwork.inventory:AddInstance(inventory, itemTable, quantity)
+	quantity = quantity or 1;
+
 	if (itemTable == nil) then
 		return false;
 	end;
@@ -154,9 +156,10 @@ function Clockwork.inventory:AddInstance(inventory, itemTable, quantity)
 		inventory[itemTable("uniqueID")] = {};
 	end;
 	
-	for i = 1, (quantity or 1) do
-		itemTable = Clockwork.item:CreateInstance(itemTable("uniqueID"));
-		inventory[itemTable("uniqueID")][itemTable("itemID")] = itemTable;
+	inventory[itemTable("uniqueID")][itemTable("itemID")] = itemTable;
+	
+	if (quantity != 1) then
+		self:AddInstance(inventory, Clockwork.item:CreateInstance(itemTable("uniqueID")), quantity - 1);
 	end;
 	
 	return itemTable;