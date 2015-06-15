d4d7785cc124ff72e82b5bd69e5d858369363f6c
Ryan <ryansaridar@hotmail.co.uk>
2015-06-15 14:31:25 +0000

    Adds faction rank functions.

--- Clockwork/garrysmod/gamemodes/clockwork/framework/libraries/server/sv_player.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/libraries/server/sv_player.lua
@@ -1,5 +1,5 @@
 --[[ 
-	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	Â© 2015 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 
 	Clockwork was created by Conna Wiles (also known as kurozael.)
@@ -1991,7 +1991,7 @@ function Clockwork.player:SetSharedVar(player, key, value, sharedTable)
 				ErrorNoHalt("[Clockwork:PlayerSharedVars] Couldn't get the sharedVars table.\n");
 				return;
 			elseif (not sharedVars[key]) then
-				ErrorNoHalt("[Clockwork:PlayerSharedVars] Couldn't find key '"..key.."' in sharedVars table. Is it registered?\n");
+				--ErrorNoHalt("[Clockwork:PlayerSharedVars] Couldn't find key '"..key.."' in sharedVars table. Is it registered?\n");
 				return;
 			end;
 			
@@ -3504,4 +3504,26 @@ end;
 function Clockwork.player:SetPlayerFlags(player, flags)
 	Clockwork.player:TakePlayerFlags(player, player:GetPlayerFlags());
 	Clockwork.player:GivePlayerFlags(player, flags);
+end;
+
+-- A function to get a player's rank within their faction.
+function Clockwork.player:GetFactionRank(player)
+	local faction = Clockwork.faction:FindByID(player:GetFaction());
+	
+	if (faction and istable(faction.ranks)) then
+		local rank;
+		
+		for k, v in pairs(faction.ranks) do
+			if (k == player:GetCharacterData("factionrank")) then
+				rank = v;
+			end;
+		end;
+		
+		return player:GetCharacterData("factionrank"), rank;
+	end;
+end;
+
+-- A function to set a player's rank within their faction.
+function Clockwork.player:SetFactionRank(player, rank)
+	player:SetCharacterData("factionrank", rank);
 end;
