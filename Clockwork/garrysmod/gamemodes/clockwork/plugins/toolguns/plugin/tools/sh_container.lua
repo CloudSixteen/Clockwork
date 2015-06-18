817e5071c57fcf031a8a6d4f76a618c9e6340f5c
Alex Savory <trurascalz@gmail.com>
2015-06-18 22:58:08 +0100

    Added Single Door tool
    Also updated to Clockwork Coding Standards & cleaned code

--- Clockwork/garrysmod/gamemodes/clockwork/plugins/toolguns/plugin/tools/sh_container.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/toolguns/plugin/tools/sh_container.lua
@@ -14,12 +14,12 @@ TOOL.UniqueID 		= "containertool";
 TOOL.Desc 			= "Do various things with containers.";
 TOOL.HelpText		= "Primary: Do Action";
 
-TOOL.ClientConVar[ "mode" ]	 			= "1"
-TOOL.ClientConVar[ "contfillscale" ]	= "1"
-TOOL.ClientConVar[ "fillcategory" ]		= "Consumables"
-TOOL.ClientConVar[ "contname" ]			= "A Container"
-TOOL.ClientConVar[ "contmessage" ]		= "A Message"
-TOOL.ClientConVar[ "contpassword" ] 	= "password"
+TOOL.ClientConVar[ "mode" ]	 			= "1";
+TOOL.ClientConVar[ "contfillscale" ]	= "1";
+TOOL.ClientConVar[ "fillcategory" ]		= "Consumables";
+TOOL.ClientConVar[ "contname" ]			= "A Container";
+TOOL.ClientConVar[ "contmessage" ]		= "A Message";
+TOOL.ClientConVar[ "contpassword" ] 	= "password";
 
 
 function TOOL:AddItems(entity)
@@ -90,7 +90,7 @@ function TOOL:SetMessage(entity)
 
 	if (IsValid(trace.Entity)) then
 		if (Clockwork.entity:IsPhysicsEntity(trace.Entity)) then
-			trace.Entity.cwMessage = self:GetClientInfo("contmessage")
+			trace.Entity.cwMessage = self:GetClientInfo("contmessage");
 			
 			Clockwork.player:Notify(player, "You have set this container's message.");
 		else
@@ -170,21 +170,21 @@ function TOOL:LeftClick( trace )
 	local container = player:GetEyeTraceNoCursor();
 
 	if (!player:IsAdmin()) then 
-		return false
+		return false;
 	end
 	
 	if (IsValid(container.Entity)) then
 		if(mode == 1) then
-			self:AddItems(container)
+			self:AddItems(container);
 		end
 		if(mode == 2) then
-			self:SetMessage(container)
+			self:SetMessage(container);
 		end
 		if(mode == 3) then
-			self:SetName(container)
+			self:SetName(container);
 		end
 		if(mode == 4) then
-			self:SetPassword(container)
+			self:SetPassword(container);
 		end
 	end
 end
@@ -194,106 +194,106 @@ if CLIENT then
 	
 	local function AddDefControls( Panel )
 
-		Panel:ClearControls()
+		Panel:ClearControls();
 	
 
-		local mode = LocalPlayer():GetInfoNum( "containertool_mode", 0 )
+		local mode = LocalPlayer():GetInfoNum( "containertool_mode", 0 );
 		
-		local list = vgui.Create("DListView")
+		local list = vgui.Create("DListView");
 
-		//17 per item + 16 for title
-		local height = 100
+
+		local height = 90;
 		
 		
 
-		list:SetSize(30,height)
-		//list:SizeToContents()
-		list:AddColumn("Tool Mode")
-		list:SetMultiSelect(false)
+		list:SetSize(30,height);
+		--list:SizeToContents()
+		list:AddColumn("Tool Mode");
+		list:SetMultiSelect(false);
 		function list:OnRowSelected(LineID, line)
 			if not (mode == LineID) then
-				RunConsoleCommand("cont_setmode", LineID)
+				RunConsoleCommand("cont_setmode", LineID);
 			end
 		end
 
 		if ( mode == 1 ) then
-			list:AddLine(" 1 **Container Filler**")
+			list:AddLine(" 1 **Container Filler**");
 		else
-			list:AddLine(" 1   Container Filler")
+			list:AddLine(" 1   Container Filler");
 		end
 		if ( mode == 2 ) then
-			list:AddLine(" 2 **Container Set Message**")
+			list:AddLine(" 2 **Container Set Message**");
 		else
-			list:AddLine(" 2   Container Set Message")
+			list:AddLine(" 2   Container Set Message");
 		end
 		if ( mode == 3 ) then
-			list:AddLine(" 3 **Container Set Name**")
+			list:AddLine(" 3 **Container Set Name**");
 		else
-			list:AddLine(" 3   Container Set Name  ")
+			list:AddLine(" 3   Container Set Name  ");
 		end
 		if ( mode == 4 ) then
-			list:AddLine(" 4 **Container Set Password**")
+			list:AddLine(" 4 **Container Set Password**");
 		else
-			list:AddLine(" 4   Container Set Password  ")
+			list:AddLine(" 4   Container Set Password  ");
 		end
 		
-		list:SortByColumn(1)
-		Panel:AddItem(list)
+		list:SortByColumn(1);
+
+		Panel:AddItem(list);
 
 		
 
 		if ( mode == 1 ) then 
-			Panel:AddControl( "Slider",  { Label	= "Item Fill Scale",
+			Panel:AddControl( "Slider",  { 
+					Label	= "Item Fill Scale",
 					Type	= "Interger",
 					Min		= 1,
 					Max		= 5,
 					Command = "containertool_contfillscale",
-					Description = "Scale of Item fill"}	 )
+					Description = "Scale of Item fill"}	 );
 
-			Panel:AddControl( "TextBox", { Label = "Category",
+			Panel:AddControl( "TextBox", { 
+									 Label = "Category",
 									 MaxLenth = "20",
-									 Command = "containertool_fillcategory" } )
+									 Command = "containertool_fillcategory" } );
 		end
 		if ( mode == 2 ) then 
-		
-
-			Panel:AddControl( "TextBox", { Label = "Message",
+			Panel:AddControl( "TextBox", { 
+									 Label = "Message",
 									 MaxLenth = "20",
-									 Command = "containertool_contmessage" } )
+									 Command = "containertool_contmessage" } );
 		end
 		if ( mode == 3) then 
-			
-
-			Panel:AddControl( "TextBox", { Label = "Name",
-									 MaxLenth = "20",
-									 Command = "containertool_contname" } )
+			Panel:AddControl( "TextBox", { 
+						Label = "Name",
+						MaxLenth = "20",
+						Command = "containertool_contname" } );
 		end
 		if ( mode == 4) then 
-			
-
-			Panel:AddControl( "TextBox", { Label = "Password",
+			Panel:AddControl( "TextBox", { 
+									 Label = "Password",
 									 MaxLenth = "20",
-									 Command = "containertool_contpassword" } )
+									 Command = "containertool_contpassword" } );
 		end
 	end
 	
 	function cont_setmode( player, tool, args )
 		if LocalPlayer():GetInfoNum( "containertool_mode", 3 ) != args[1] then
-			RunConsoleCommand("containertool_mode", args[1])
-			timer.Simple(0.05, function() cont_updatepanel() end ) 
+			RunConsoleCommand("containertool_mode", args[1]);
+			timer.Simple(0.05, function() cont_updatepanel(); end );
 		end
 	end
-	concommand.Add( "cont_setmode", cont_setmode )
+	concommand.Add( "cont_setmode", cont_setmode );
 
 	function cont_updatepanel()
-		local Panel = controlpanel.Get( "containertool" )
+		local Panel = controlpanel.Get( "containertool" );
 		if (!Panel) then return end
-		AddDefControls( Panel )
+		AddDefControls( Panel );
 	end
-	concommand.Add( "cont_updatepanel", cont_updatepanel )
+	concommand.Add( "cont_updatepanel", cont_updatepanel );
 
 	function TOOL.BuildCPanel( Panel )
-		AddDefControls( Panel )
+		AddDefControls( Panel );
 	end
 end
 	
