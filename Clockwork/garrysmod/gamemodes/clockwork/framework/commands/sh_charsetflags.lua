56df809a6ac277c4381e3bb8151c90bf0893112a
kurozael <kurozael@gmail.com>
2015-08-27 14:17:17 +0100

    Some language strings + fixed consistency of lang-string naming (langDesc to LangDesc, etc) Little by little...

--- Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_charsetflags.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_charsetflags.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2015 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 
 	Clockwork was created by Conna Wiles (also known as kurozael.)
@@ -30,7 +30,7 @@ function COMMAND:OnRun(player, arguments)
 		
 		Clockwork.player:NotifyAll(player:Name().." set "..target:Name().."'s flags to "..arguments[2]..".");
 	else
-		Clockwork.player:Notify(player, arguments[1].." is not a valid character!");
+		Clockwork.player:Notify(player, L(player, "NotValidCharacter", arguments[1]));
 	end;
 end;
 
--- Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_it.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_it.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2015 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 
 	Clockwork was created by Conna Wiles (also known as kurozael.)
@@ -17,13 +17,13 @@ COMMAND.arguments = 1;
 -- Called when the command has been run.
 function COMMAND:OnRun(player, arguments)
 	local text = table.concat(arguments, " ");
-	
+
 	if (string.utf8len(text) < 8) then
-		Clockwork.player:Notify(player, "You did not specify enough text!");
-		
+		Clockwork.player:Notify(player, L(player, "NotEnoughText"));
+
 		return;
 	end;
-	
+
 	player:SetCharacterData("Test", "CHANGED");
 
 	Clockwork.chatBox:AddInTargetRadius(player, "it", text, player:GetPos(), Clockwork.config:Get("talk_radius"):Get() * 2);
