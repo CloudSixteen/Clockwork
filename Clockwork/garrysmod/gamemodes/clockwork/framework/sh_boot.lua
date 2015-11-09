1131c3b98e546f2edb84e44b4bfb137c05f164c6
NightAngel <annacraig1455@hotmail.com>
2015-11-09 06:05:21 -0500

    Alpha 0.94.18
    - Added 'PostCommandUsed' hook called after a command succeeds in
    running.

--- Clockwork/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
@@ -28,8 +28,8 @@ end;
 
 Clockwork.ClockworkFolder = Clockwork.ClockworkFolder or GM.Folder;
 Clockwork.SchemaFolder = Clockwork.SchemaFolder or GM.Folder;
-Clockwork.KernelVersion = "0.94.17";
-Clockwork.KernelBuild = "beta"
+Clockwork.KernelVersion = "0.94.18";
+Clockwork.KernelBuild = "alpha"
 Clockwork.DeveloperVersion = true;
 Clockwork.Website = "http://kurozael.com";
 Clockwork.Author = "kurozael";
