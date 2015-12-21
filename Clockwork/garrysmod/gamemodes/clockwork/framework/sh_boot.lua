d186c8c3ff58c8e75909d6ad318ac71369f79eaa
NightAngel <annacraig1455@hotmail.com>
2015-12-21 13:53:08 -0500

    Updating Version

--- Clockwork/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
@@ -28,7 +28,7 @@ end;
 
 Clockwork.ClockworkFolder = Clockwork.ClockworkFolder or GM.Folder;
 Clockwork.SchemaFolder = Clockwork.SchemaFolder or GM.Folder;
-Clockwork.KernelVersion = "0.94.74";
+Clockwork.KernelVersion = "0.94.75";
 Clockwork.KernelBuild = "alpha"
 Clockwork.DeveloperVersion = true;
 Clockwork.Website = "http://kurozael.com";
