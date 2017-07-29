aa15cd0a71f3c1fcf08789ac3d187dee9d04225a
EnderHero <TheRealEnderHero@users.noreply.github.com>
2017-07-29 13:02:52 +0200

    Create sh_french.lua
    It seems to be okay.

---
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/language/sh_french.lua
@@ -0,0 +1,61 @@
+-- [[
+	-- -- © 2015 CloudSixteen.com do not share, re-distribute or modify
+	-- -- without permission of its author (kurozael@gmail.com).
+
+	-- -- Clockwork was created by Conna Wiles (also known as kurozael.)
+	-- -- http://cloudsixteen.com/license/clockwork.html
+-- ]]
+
+CW_FRENCH = Clockwork.lang:GetTable("Français");
+
+CW_FRENCH["CannotChangeClassFor"] = "Vous ne pouvez pas changer de classe pendant encore #1 seconde(s) !";
+CW_FRENCH["CannotActionRightNow"] = "Vous ne pouvez pas faire cette action pour le moment !";
+CW_FRENCH["DroppedItemsOtherChar"] = "Vous ne pouvez pas prendre des objets que vous avez jetté avec un autre personnage !";
+CW_FRENCH["DroppedCashOtherChar"] = "Vous ne pouvez pas prendre #1 vous l'avez jetté avec un autre personnage !";
+CW_FRENCH["CannotPurchaseAnotherDoor"] = "Vous ne pouvez pas acheter une autre porte !";
+CW_FRENCH["EntityOptionWaitTime"] = "Vous ne pouvez pas utiliser une autre entité aussi vite !";
+CW_FRENCH["YouNeedAnother"] = "Vous avez besoin encore de #1 !";
+CW_FRENCH["NotEnoughText"] = "Vous n'avez pas précisé assez de texte !";
+CW_FRENCH["ConfigVariablesPrinted"] = "Les variables de configuration ont été imprimées dans la console.";
+
+CW_FRENCH["PlayerForceDemoted"] = "#1 a forcé la rétrogradation de #2 au rang de '#3'.";
+CW_FRENCH["PlayerDemotedPlayer"] = "#1 a rétrogradé #2 au rang de '#3'.";
+CW_FRENCH["DemotePermsNeeded"] = "Vous n'avez pas la permission de rétrograder ce joueur.";
+CW_FRENCH["DemoteFactionOnly"] = "Vous pouvez seulement rétrograder quelqu'un de votre propre faction !";
+CW_FRENCH["YouCannotDemotePlayer"] = "Vous ne pouvez pas rétrogradé ce joueur !";
+CW_FRENCH["ForceDemoteAdminNeeded"] = "Vous devez être un admin ou un superadmin pour forcer la rétrogradation !";
+
+CW_FRENCH["PlayerForcePromoted"] = "#1 a forcé la promotion de #2 au rang de '#3'.";
+CW_FRENCH["PlayerPromotedPlayer"] = "#1 a promu #2 au rang de '#3'.";
+CW_FRENCH["PromotePermsNeeded"] = "Vous n'avez pas la permission de promouvoir ce joueur.";
+CW_FRENCH["PromoteFactionOnly"] = "Vous pouvez seulement promouvoir quelqu'un de votre propre faction !";
+CW_FRENCH["YouCannotPromotePlayer"] = "Vous ne pouvez pas promouvoir ce joueur !";
+CW_FRENCH["ForcePromoteAdminNeeded"] = "Vous devez être un admin ou un superadmin pour forcer la promotion !";
+
+CW_FRENCH["PlayerRemovedFromWhitelist"] = "#1 a retiré #2 de la whitelist de #3.";
+CW_FRENCH["PlayerAddedToWhitelist"] = "#1 a ajouté #2 à la whitelist de #3.";
+CW_FRENCH["PlayerAlreadyOnWhitelist"] = "#1 est déjà dans la whitelist de #2 !";
+CW_FRENCH["FactionDoesNotHaveWhitelist"] = "#1 n'a pas de whitelist !";
+CW_FRENCH["PlayerNotOnWhitelist"] = "#1 n'est pas dans la whitelist de #2 !";
+
+CW_FRENCH["FactionIsNotValid"] = "#1 n'est pas une faction valide !";
+
+CW_FRENCH["PlayerConfigSetNextRestart"] = "#1 a mis #2#3 à '#4' pour le prochain redémarrage.";
+CW_FRENCH["PlayerConfigSet"] = "#1 a mis #2#3 à '#4'.";
+CW_FRENCH["PlayerBannedPlayer"] = "#1 a banni le personnage '#2'.";
+CW_FRENCH["PlayerIsProtected"] = "#1 est protégé !";
+CW_FRENCH["PlayerHasFlags"] = "Ce joueur a le drapeau #1.";
+CW_FRENCH["NotValidMap"] = "#1 n'est pas une carte valide !";
+CW_FRENCH["CannotHolsterWeapon"] = "Vous ne pouvez pas ranger cette arme !";
+CW_FRENCH["CannotDropWeapon"] = "Vous ne pouvez pas lâcher cette arme !";
+CW_FRENCH["CannotUseWeapon"] = "Vous ne pouvez pas utiliser cette arme !";
+CW_FRENCH["ConfigUnableToSet"] = "#1 n'a pas pu être mis !";
+CW_FRENCH["ConfigIsStaticKey"] = "#1 est une clé de configuration statique !";
+CW_FRENCH["ConfigKeyNotValid"] = "#1 n'est pas une clé de configuration valide !";
+CW_FRENCH["NotValidCharacter"] = "#1 n'est pas un personnage valide !";
+CW_FRENCH["NotValidPlayer"] = "#1 n'est pas un joueur valide !";
+CW_FRENCH["Framework"] = "Framework";
+CW_FRENCH["ChatBox"] = "Boîte de discussion";
+CW_FRENCH["Theme"] = "Thème";
+CW_FRENCH["Language"] = "Langue";
+CW_FRENCH["LangDesc"] = "La langue actuellement choisie.";
