1421886f1bab3579302ee9734bdf572778258194
Tensa <zenkins26@gmail.com>
2017-07-30 22:08:31 +0900

    Create korean language file.

---
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/language/sh_korean.lua
@@ -0,0 +1,63 @@
+--[[
+	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+	
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
+	
+	This language file is created by Tensa. (https://github.com/10sa or http://steamcommunity.com/id/Developer_Tensa/)
+--]]
+
+CW_KOREAN = Clockwork.lang:GetTable("Korean");
+
+CW_KOREAN["CannotChangeClassFor"] = "#1 초 동안은 클래스를 변경할 수 없습니다.";
+CW_KOREAN["CannotActionRightNow"] = "당신은 현재 이 행동을 할 수 없습니다.";
+CW_KOREAN["DroppedItemsOtherChar"] = "당신은 당신의 다른 캐릭터가 드롭한 아이템을 획득할 수 없습니다.";
+CW_KOREAN["DroppedCashOtherChar"] = "당신은 당신의 다른 캐릭터가 드롭한 #1 를 획득할 수 없습니다.";
+CW_KOREAN["CannotPurchaseAnotherDoor"] = "당신은 더이상 다른 문을 구매할 수 없습니다.";
+CW_KOREAN["EntityOptionWaitTime"] = "다른 엔티티를 빠르게 사용할 수 없습니다.";
+CW_KOREAN["YouNeedAnother"] = "당신은 다른 #1 이 필요합니다.";
+CW_KOREAN["NotEnoughText"] = "더 많은 내용이 필요합니다.";
+CW_KOREAN["ConfigVariablesPrinted"] = "설정 값들이 당신의 콘솔에 출력되었습니다.";
+
+CW_KOREAN["PlayerForceDemoted"] = "#1 님이 #2 님을 '#3' 로 설정했습니다.";
+CW_KOREAN["PlayerDemotedPlayer"] = "#1 님이 #2 님을 '#3' 로 설정하였습니다.";
+CW_KOREAN["DemotePermsNeeded"] = "당신은 이 플레이어의 등급을 설정하기에 충분한 권한을 가지고 있지 않습니다.";
+CW_KOREAN["DemoteFactionOnly"] = "당신의 진영에서만 누군가의 등급을 설정할 수 있습니다.";
+CW_KOREAN["YouCannotDemotePlayer"] = "당신은 이 플레이어의 등급을 설정할 수 없습니다.";
+CW_KOREAN["ForceDemoteAdminNeeded"] = "당신은 이 명령어를 사용할 권한이 없습니다.";
+
+CW_KOREAN["PlayerForcePromoted"] = "#1 님이 #2 님을 '#3' 로 설정했습니다.";
+CW_KOREAN["PlayerPromotedPlayer"] = "#1 님이 #2 님을 '#3' 로 설정했습니다.";
+CW_KOREAN["PromotePermsNeeded"] = "당신은 이 플레이어의 등급을 설정하기에 충분한 권한을 가지고 있지 않습니다.";
+CW_KOREAN["PromoteFactionOnly"] = "당신의 진영에서만 누군가의 등급을 설정할 수 있습니다.";
+CW_KOREAN["YouCannotPromotePlayer"] = "당신은 이 플레이어의 등급을 설정할 수 없습니다.";
+CW_KOREAN["ForcePromoteAdminNeeded"] = "당신은 이 명령어를 사용할 권한이 없습니다.";
+
+CW_KOREAN["PlayerRemovedFromWhitelist"] = "#1 님이 #2 님으로 부터 #3 팩션을 제거하였습니다.";
+CW_KOREAN["PlayerAddedToWhitelist"] = "#1 님이 #2 님에게 #3 팩션을 지급하였습니다.";
+CW_KOREAN["PlayerAlreadyOnWhitelist"] = "#1 님은 이미 #2 팩션을 가지고 있습니다.";
+CW_KOREAN["FactionDoesNotHaveWhitelist"] = "#1 님은 해당 팩션을 가지고 있지 않습니다.";
+CW_KOREAN["PlayerNotOnWhitelist"] = "#1 님은 #2 팩션을 가지고 있지 않습니다.";
+
+CW_KOREAN["FactionIsNotValid"] = "#1 는 올바르지 않은 팩션입니다.";
+
+CW_KOREAN["PlayerConfigSetNextRestart"] = "#1 님이 다음 재시작을 위해 #2#3 를 '#4' 설정으로 변경하였습니다.";
+CW_KOREAN["PlayerConfigSet"] = "#1 님이 #2#3 를 '#4' 로 설정하였습니다.";
+CW_KOREAN["PlayerBannedPlayer"] = "#1 님이 '#2' 캐릭터를 밴 하였습니다.";
+CW_KOREAN["PlayerIsProtected"] = "#1 는 보호되고 있습니다.";
+CW_KOREAN["PlayerHasFlags"] = "이 플레이어는 #1 플레그를 소유 중입니다.";
+CW_KOREAN["NotValidMap"] = "#1 는 올바르지 않은 맵입니다.";
+CW_KOREAN["CannotHolsterWeapon"] = "이 무기는 장착 해제할 수 없습니다.";
+CW_KOREAN["CannotDropWeapon"] = "이 무기는 떨어트릴 수 없습니다.";
+CW_KOREAN["CannotUseWeapon"] = "당신은 이 무기를 사용할 수 없습니다.";
+CW_KOREAN["ConfigUnableToSet"] = "#1 를 설정할 수 없습니다.";
+CW_KOREAN["ConfigIsStaticKey"] = "#1 는 정적 설정 값입니다.";
+CW_KOREAN["ConfigKeyNotValid"] = "#1 는 올바르지 않은 설정 키입니다.";
+CW_KOREAN["NotValidCharacter"] = "#1 는 올바르지 않은 캐릭터입니다.";
+CW_KOREAN["NotValidPlayer"] = "#1 는 올바르지 않은 플레이어입니다.";
+CW_KOREAN["Framework"] = "프레임워크";
+CW_KOREAN["ChatBox"] = "체터박스";
+CW_KOREAN["Theme"] = "테마";
+CW_KOREAN["Language"] = "언어";
+CW_KOREAN["LangDesc"] = "현재 보여지고 있는 언어입니다.";
