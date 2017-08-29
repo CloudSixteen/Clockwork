12a32b2a624d784bc176b67c70e2d7e0cb2cd519
minikanyas <minikanyas@gmail.com>
2017-08-29 15:48:19 +0200

    Added spanish translation for storage

---
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/storage/plugin/language/sh_spanish.lua
@@ -0,0 +1,17 @@
+--[[
+	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
+--]]
+
+CW_SPANISH = Clockwork.lang:GetTable("Spanish");
+
+CW_SPANISH["YouSetContainerPassword"] = "Has cambiado la contraseña del contenedor.";
+CW_SPANISH["YouSetContainerMessage"] = "Has cambiado el mensaje del contenedor.";
+CW_SPANISH["YouRemovedContainerPassword"] = "Has eliminado la contraseña del contenedor.";
+CW_SPANISH["ContainerFilledWithRandomItems"] = "El contenedor se ha llenado con objetos aleatorios.";
+CW_SPANISH["ContainerCategoryNotExist"] = "Esta categoría de objetos no existe!";
+CW_SPANISH["ContainerNotValidScale"] = "Esta escala no es válida!";
+CW_SPANISH["ContainerNotValid"] = "Esto no es un contenedor válido!";
+CW_SPANISH["ContainerIncorrectPassword"] = "Has introducido una contraseña incorrecta!";
