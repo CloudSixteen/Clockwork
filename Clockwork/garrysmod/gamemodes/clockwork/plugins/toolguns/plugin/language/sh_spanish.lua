df95d36adb8a1ff75dc999f5028e2af214bdacf0
minikanyas <minikanyas@gmail.com>
2017-08-29 15:52:12 +0200

    Added spanish translation for toolguns

---
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/toolguns/plugin/language/sh_spanish.lua
@@ -0,0 +1,29 @@
+--[[
+	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
+--]]
+
+CW_SPANISH = Clockwork.lang:GetTable("Spanish");
+
+CW_SPANISH["SurfaceTextToolDesc"] = "Añade un texto en el mapa. Puede ser coloreado y con diferente tamaño.";
+CW_SPANISH["SurfaceTextToolAddText"] = "Añadir/Eliminar Texto";
+CW_SPANISH["SurfaceTextToolSizeText"] = "Tamaño del texto";
+CW_SPANISH["SurfaceTextToolColorText"] = "Color del texto";
+
+CW_SPANISH["ContainerToolMode1"] = "1. Llenar Contenedor";
+CW_SPANISH["ContainerToolMode2"] = "2. Cambiar Mensaje Contenedor";
+CW_SPANISH["ContainerToolMode3"] = "3. Cambiar Nombre Contenedor";
+CW_SPANISH["ContainerToolMode4"] = "4. Cambiar Contraseña Contenedor";
+CW_SPANISH["ContainerToolSelectedMode1"] = "1. ** Llenar Contenedor **";
+CW_SPANISH["ContainerToolSelectedMode2"] = "2. ** Cambiar Mensaje Contenedor **";
+CW_SPANISH["ContainerToolSelectedMode3"] = "3. ** Cambiar Nombre Contenedor **";
+CW_SPANISH["ContainerToolSelectedMode4"] = "4. ** Cambiar Contraseña Contenedor **";
+
+CW_SPANISH["ContainerToolScaleOfFillName"] = "Escala Relleno Objetos";
+CW_SPANISH["ContainerToolScaleOfFillDesc"] = "La escala de relleno de objetos a utilizar para llenar el contenedor.";
+CW_SPANISH["ContainerToolCategoryName"] = "Categoría";
+CW_SPANISH["ContainerToolMessage"] = "Mensaje";
+CW_SPANISH["ContainerToolName"] = "Nombre";
+CW_SPANISH["ContainerToolPassword"] = "Contraseña";
