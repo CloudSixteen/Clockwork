4bb57b7b017e3109a6d352de9a909e7f4c34650e
minikanyas <minikanyas@gmail.com>
2017-08-30 00:24:42 +0200

    Corrected misspellings Redone most of translations (no more "Google Translate" looking ones) Adapted the strings to the own language (no literal translations, they make sense.) Changed encoding to UTF-8 Translated all the framework plugins to spanish

---
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/spawnpoints/plugin/language/sh_spanish.lua
@@ -0,0 +1,21 @@
+--[[
+	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
+--]]
+
+CW_SPANISH = Clockwork.lang:GetTable("Spanish");
+
+CW_SPANISH["YouRemovedNameSpawnpoints"] = "Has eliminado #1 #2 punto(s) de spawn.";
+CW_SPANISH["YouRemovedDefaultSpawnpoints"] = "Has eliminado un total de #1 spawn(s) por defecto.";
+CW_SPANISH["NoNameSpawnpointsNearHere"] = "No hay #1 puntos de spawn cerca de esta posición.";
+CW_SPANISH["NoDefaultSpawnpointsNearHere"] = "No hay puntos de spawn por defecto cerca de esta posición.";
+CW_SPANISH["NoDefaultSpawnpoints"] = "Actualmente, no hay puntos de spawn por defecto.";
+CW_SPANISH["NoNameSpawnpoints"] = "Actualmente, no hay ningún #1 punto de spawn.";
+CW_SPANISH["NotValidClassOrFaction"] = "Esta clase o facción no es válida!";
+CW_SPANISH["YouAddedNameSpawnpoint"] = "Has creado un punto de spawn para #1.";
+CW_SPANISH["YouAddedDefaultSpawnpoint"] = "Has creado un punto de spawn por defecto.";
+
+CW_SPANISH["ShowSpawnPoints"] = "Mostrar Puntos Spawn";
+CW_SPANISH["ShowSpawnPointsDesc"] = "Mostrar o ocultar los puntos de spawn en el Admin ESP.";
