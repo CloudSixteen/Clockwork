afac0b2aabb8e4da9499a2f83984a1aea1004ead
minikanyas <minikanyas@gmail.com>
2017-08-29 14:28:55 +0200

    Add files via upload

---
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/crossserverchat/plugin/language/sh_spanish.lua
@@ -0,0 +1,13 @@
+--[[
+	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
+--]]
+
+CW_SPANISH = Clockwork.lang:GetTable("Spanish");
+
+CW_SPANISH["CrossServerChatEnabled"] = "Activar Chat Entre Servidores";
+CW_SPANISH["CrossServerChatEnabledDesc"] = "Activar o desactivar la posibilidad de conectar el chat entre diferentes servidores.";
+CW_SPANISH["CrossServerChatName"] = "Nombre Servidor Chat Entre Servidores";
+CW_SPANISH["CrossServerChatNameDesc"] = "Un nombre único utilizado para referirse al chat entre diferentes servidores.";
