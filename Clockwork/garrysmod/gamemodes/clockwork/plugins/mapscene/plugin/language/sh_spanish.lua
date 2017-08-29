dcf4b3af32dae716e62894543ca5dbfd40c6119a
minikanyas <minikanyas@gmail.com>
2017-08-29 15:07:18 +0200

    Added spanish translation for displaytyping

---
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/mapscene/plugin/language/sh_spanish.lua
@@ -0,0 +1,13 @@
+--[[
+	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
+--]]
+
+CW_SPANISH = Clockwork.lang:GetTable("Spanish");
+
+CW_SPANISH["MapSceneRemoved"] = "Has eliminado un total de #1 escena(s).";
+CW_SPANISH["MapSceneAdded"] = "Has creado una escena.";
+CW_SPANISH["MapSceneNoneNearPosition"] = "No hay escenas cerca de esta posición.";
+CW_SPANISH["MapSceneNoneExist"] = "Actualmente, no existe ninguna escena.";
