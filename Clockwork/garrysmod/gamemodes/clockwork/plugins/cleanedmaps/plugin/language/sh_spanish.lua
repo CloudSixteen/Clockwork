e589388cdd243e7525e802e2616532a8ff281169
minikanyas <minikanyas@gmail.com>
2017-08-29 14:23:46 +0200

    Add files via upload

---
+++ Clockwork/garrysmod/gamemodes/clockwork/plugins/cleanedmaps/plugin/language/sh_spanish.lua
@@ -0,0 +1,11 @@
+--[[
+	© 2015 CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+	Clockwork was created by Conna Wiles (also known as kurozael.)
+	http://cloudsixteen.com/license/clockwork.html
+--]]
+
+CW_SPANISH = Clockwork.lang:GetTable("Spanish");
+
+CW_SPANISH["RemoveMapPhysics"] = "Eliminar Físicas Mapa";
+CW_SPANISH["RemoveMapPhysicsDesc"] = "Eliminar o no eliminar las entidades con físicas al realizar un cargado de mapa.";
