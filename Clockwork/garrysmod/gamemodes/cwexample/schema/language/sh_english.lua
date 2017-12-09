e0c7c35badfb6cdf45984b6c693c175d98aced64
kurozael <kurozael@gmail.com>
2017-12-09 23:30:31 +0000

    Lang

--- Clockwork/garrysmod/gamemodes/cwexample/schema/language/sh_english.lua
+++ Clockwork/garrysmod/gamemodes/cwexample/schema/language/sh_english.lua
@@ -8,3 +8,5 @@ CW_ENGLISH = Clockwork.lang:GetTable("English");
 CW_ENGLISH["ExampleFaction"] = "Example Faction";
 CW_ENGLISH["ItemExample"] = "Example Item";
 CW_ENGLISH["ItemExampleDesc"] = "An example item.";
+CW_ENGLISH["AttributeExample"] = "Example Attribute";
+CW_ENGLISH["AttributeExampleDesc"] = "A short description for the example attribute.";
