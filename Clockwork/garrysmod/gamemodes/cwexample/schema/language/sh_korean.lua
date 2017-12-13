957b73dbcafeca59f77afb2d7e8844deb55b18bf
Tensa <zenkins26@gmail.com>
2017-12-13 18:50:04 +0900

    Update Korean translation.

---
+++ Clockwork/garrysmod/gamemodes/cwexample/schema/language/sh_korean.lua
@@ -0,0 +1,12 @@
+--[[
+	This project is created with the Clockwork framework by Cloud Sixteen.
+	http://cloudsixteen.com
+--]]
+
+CW_KOREAN = Clockwork.lang:GetTable("Korean");
+
+CW_KOREAN["ExampleFaction"] = "예시 팩션";
+CW_KOREAN["ItemExample"] = "예시 아이템";
+CW_KOREAN["ItemExampleDesc"] = "예시 아이템입니다.";
+CW_KOREAN["AttributeExample"] = "예시 능력치";
+CW_KOREAN["AttributeExampleDesc"] = "능력치를 위한 짧은 설명입니다.";
