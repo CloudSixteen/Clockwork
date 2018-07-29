12a4b74abb1fc8317c22162d3eba36a5bdc55efe
Viz <vizurez@gmail.com>
2018-07-29 14:12:20 +0100

    Example faction fix
    FACTION.isWhitelisted should be FACTION.whitelisted

--- Clockwork/garrysmod/gamemodes/cwexample/schema/factions/sh_example.lua
+++ Clockwork/garrysmod/gamemodes/cwexample/schema/factions/sh_example.lua
@@ -5,7 +5,7 @@
 
 local FACTION = Clockwork.faction:New("Example Faction");
 
-FACTION.isWhitelisted = false; -- Do we need to be whitelisted to select this faction?
+FACTION.whitelisted = false; -- Do we need to be whitelisted to select this faction?
 FACTION.useFullName = true; -- Do we allow players to enter a full name, otherwise it only lets them select a first and second.
 FACTION.material = "path/to/material"; -- The path to the faction material (shown on the creation screen).
 FACTION.models = {
