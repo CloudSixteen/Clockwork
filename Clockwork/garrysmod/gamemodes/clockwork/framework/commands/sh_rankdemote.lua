d25e1d8b7af4a0fc344ede6158b51b3f95f64314
Ryan <rsaridar@outlook.com>
2018-11-29 22:47:34 +0000

    Revert changes made to master

--- Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_rankdemote.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_rankdemote.lua
@@ -46,9 +46,9 @@ function COMMAND:OnRun(player, arguments)
 			if (Clockwork.player:CanDemote(player, target)) then
 				local rank, rankTable = target:GetFactionRank();
 
-				Clockwork.player:SetFactionRank(target, Clockwork.faction:GetLowerRank(target:GetFaction(), rankTable));
+				Clockwork.player:SetFactionRank(target, Clockwork.faction:GetHigherRank(target:GetFaction(), rankTable));
 
-				Clockwork.player:NotifyAll({"PlayerDemotedPlayer", player:Name(), target:Name(), (target:GetFactionRank())});
+				Clockwork.player:NotifyAll({"PlayerForceDemoted", player:Name(), target:Name()});
 			else
 				Clockwork.player:Notify(player, {"DemotePermsNeeded"});
 			end;
