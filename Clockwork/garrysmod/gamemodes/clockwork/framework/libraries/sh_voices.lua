c87cd5ca42e1a532937e240dee148f311ea22b1e
Ryan <rsaridar@outlook.com>
2019-02-24 01:15:52 +0000

    Allowed hook continuation
    Allowed ChatBoxAdjustInfo hooks to run after the voices hook, for further info modification

--- Clockwork/garrysmod/gamemodes/clockwork/framework/libraries/sh_voices.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/libraries/sh_voices.lua
@@ -188,15 +188,11 @@ function Clockwork.voices:ChatBoxAdjustInfo(info)
 
 							if (v2.phrase == nil or v2.phrase == "") then
 								info.visible = false;
-								
-								if (SERVER) then
-									Clockwork.kernel:PrintLog(LOGTYPE_GENERIC, {"LogPlayerSays", info.speaker:Name(), info.text});
-								end;
 							else
 								info.text = v2.phrase;
 							end;
-						
-							return true;
+
+							return;
 						end;
 					end;
 				end;
