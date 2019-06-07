7e2f2fe9efee9910d0c6c8a6316628d7c13bec49
ExtReMLapin <ExtReMLapin@users.noreply.github.com>
2019-06-07 13:57:27 +0200

    fixed double if statement

--- Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_chargetup.lua
+++ Clockwork/garrysmod/gamemodes/clockwork/framework/commands/sh_chargetup.lua
@@ -15,11 +15,10 @@ COMMAND.flags = CMD_DEFAULT;
 
 -- Called when the command has been run.
 function COMMAND:OnRun(player, arguments)
-	if (player:GetRagdollState() == RAGDOLL_FALLENOVER and player:GetSharedVar("FallenOver") and Clockwork.player:GetAction(player) != "unragdoll") then
-		if (Clockwork.plugin:Call("PlayerCanGetUp", player)) then
-			Clockwork.player:SetUnragdollTime(player, 5);
-			player:SetSharedVar("FallenOver", false);
-		end;
+	if (player:GetRagdollState() == RAGDOLL_FALLENOVER and player:GetSharedVar("FallenOver") and Clockwork.player:GetAction(player) != "unragdoll")
+	and Clockwork.plugin:Call("PlayerCanGetUp", player) then
+		Clockwork.player:SetUnragdollTime(player, 5);
+		player:SetSharedVar("FallenOver", false);
 	end;
 end;
 
