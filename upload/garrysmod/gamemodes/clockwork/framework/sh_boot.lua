f6a3b049b4f5932cf2fd1ddecaf705d44f9d6fa6
Conna Wiles <kurozael@gmail.com>
2019-10-27 22:57:44 +0000

    Update sh_boot.lua

--- upload/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
+++ upload/garrysmod/gamemodes/clockwork/framework/sh_boot.lua
@@ -29,7 +29,6 @@ end;
 Clockwork.ClockworkFolder = Clockwork.ClockworkFolder or GM.Folder;
 Clockwork.SchemaFolder = Clockwork.SchemaFolder or GM.Folder;
 Clockwork.KernelVersion = "0.99";
-Clockwork.KernelBuild = "beta"
 Clockwork.DeveloperVersion = true;
 Clockwork.Website = "http://kurozael.com";
 Clockwork.Author = "kurozael";
