d8cc4e9f0a04ae6b612c323fe683311968bfc887
Conna Wiles <kurozael@gmail.com>
2019-12-03 19:27:41 +0000

    Update sh_turkish.lua

--- upload/garrysmod/gamemodes/clockwork/framework/language/sh_turkish.lua
+++ upload/garrysmod/gamemodes/clockwork/framework/language/sh_turkish.lua
@@ -1,6 +1,7 @@
 --[[
 	© CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
+
 	Clockwork was created by Conna Wiles (also known as kurozael.)
 	http://cloudsixteen.com/license/clockwork.html
 --]]
